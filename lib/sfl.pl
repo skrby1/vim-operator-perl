1127bb1d829f07eef7f97289ad2f4ecb6b2ba32e
skrby1 <info@skrby1.com>
2021-01-21 01:14:55 +0900

    minor improvement

--- lib/sfl.pl
+++ lib/sfl.pl
@@ -5,12 +5,15 @@
 use strict;
 use List::Util;
 
+my $delimiter = '';
+$delimiter = $ARGV[0] if defined $ARGV[0];
+
 my $out;
 while(<STDIN>){
   if(chomp()){
-    $out .= join('', List::Util::shuffle(split('', $_))). "\n";
+    $out .= join($delimiter, List::Util::shuffle(split($delimiter, $_))). "\n";
   } else {
-    $out .= join('', List::Util::shuffle(split('', $_)));
+    $out .= join($delimiter, List::Util::shuffle(split($delimiter, $_)));
   }
 }
 
